f7edaab77128727fa2b4a6d76919f0d58db926bd
Ricebeater <110720098+Ricebeater@users.noreply.github.com>
2025-11-03 00:05:11 +0700

    Upload file button
    user need to upload answer and key to get respond

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import runpy
-from pathlib import Path
-from export import export_to_excel
-
-export_to_excel()
-
-student_answer = st.file_uploader(
-    "Upload your respond", type=["csv"]
-    )
-
-key_answer = st.file_uploader(
-    "Upload your key", type=["csv"]
-    )
-
-
-df = pd.read_excel("graded_result.xlsx")
-
-config = {
-    "similarity": st.column_config.ProgressColumn()
-}
+from export import load_data
 
 st.set_page_config(page_title="SmartCheck Grading System", layout="wide")
 
@@ -30,4 +11,20 @@ with col2:
     st.title("📋 SmartCheck Grading System 📋")
     st.write("Upload student answers and answer key to grade responses.")
 
-st.dataframe(df, width="stretch", height=400, column_config=config)
+
+student_answer = st.file_uploader(
+    "Upload your respond", type=["csv", "xlsx"]
+    )
+
+key_answer = st.file_uploader(
+    "Upload your key", type=["csv", "xlsx"]
+    )
+
+if student_answer is not None and key_answer is not None:
+    load_data(student_answer, key_answer)
+    df = pd.read_excel("graded_result.xlsx")
+    config = {
+        "similarity": st.column_config.ProgressColumn()
+        }
+    st.dataframe(df, width="stretch", height=400, column_config=config)
+
--- export.py
+++ export.py
@@ -3,34 +3,33 @@ import pandas as pd
 from scoring import answer_similarity, need_review, scroering
 
 # Read CSV files and strip whitespace from column names
-df_answer = pd.read_csv("answer.csv", skipinitialspace=True)
-df_key = pd.read_csv("answer_key.csv", skipinitialspace=True)
+def load_data(student_answer, key_answer):
+    df_answer = pd.read_csv(student_answer, skipinitialspace=True)
+    df_key = pd.read_csv(key_answer, skipinitialspace=True)
+    
+    # Clean column names by stripping whitespace
+    df_answer.columns = df_answer.columns.str.strip()
+    df_key.columns = df_key.columns.str.strip()
 
-# Clean column names by stripping whitespace
-df_answer.columns = df_answer.columns.str.strip()
-df_key.columns = df_key.columns.str.strip()
-
-key_dict = dict(
+    key_dict = dict(
     zip(
         df_key["questionID"],
         df_key["correctAnswer"]
         )
     )
-results = []
+    results = []
 
-for _, row in df_answer.iterrows():
-    student_id = row["studentID"]
-    question_id = row["questionID"]
-    student_answer = row["studentAnswer"]
-    correct_answer = key_dict.get(question_id, "")
+    for _, row in df_answer.iterrows():
+        student_id = row["studentID"]
+        question_id = row["questionID"]
+        student_answer = row["studentAnswer"]
+        correct_answer = key_dict.get(question_id, "")
     
-    similarity = answer_similarity(student_answer, correct_answer)
-    score = scroering(similarity)
-    review = need_review(similarity)
+        similarity = answer_similarity(student_answer, correct_answer)
+        score = scroering(similarity)
+        review = need_review(similarity)
     
-    results.append
-    (
-        {
+        results.append({
             "studentID": student_id,
             "questionID": question_id,
             "studentAnswer": student_answer,
@@ -38,13 +37,13 @@ for _, row in df_answer.iterrows():
             "similarity": round(similarity, 2),
             "score": score,
             "need_review": review
-        }
-    )
+        })
 
-df_results = pd.DataFrame(results)
-df_results.to_csv("results.csv", index=False)
-
-def export_to_excel():
+    df_results = pd.DataFrame(results)
+    df_results.to_csv("results.csv", index=False)
     df_results.to_excel("graded_result.xlsx", index=False)
+    
+    return df_results
+    
 
-export_to_excel()
+
